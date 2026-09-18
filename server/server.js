87d096296d4940ffbbca57b07f375d41e5402a02
Mihir Patel <mk04patel@gmail.com>
2026-09-17 21:02:11 -0500

    Added /mobs endpoint

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import mobsRouter from './routes/mobRoutes.js';
 
 const PORT = 3001;
 const app = express();
 
 app.use(express.static('./public'));
 app.use('/scripts', express.static('./public/scripts'));
+app.use('/mobs', mobsRouter);
 
 app.get('/', (req, res) => {
   res.status(200).send('<h1>The Minecraft Mob API</h1>')
