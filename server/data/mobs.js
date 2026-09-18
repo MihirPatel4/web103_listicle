4e3f6c68a6d864f0481edc7bcb4aceb6c3cef65b
Mihir Patel <mk04patel@gmail.com>
2026-09-17 20:49:21 -0500

    Created mob data

---
+++ server/data/mobs.js
@@ -0,0 +1,52 @@
+const mobData = [
+  {
+    "id": 0,
+    "name": "Zombie",
+    "health": 20,
+    "type": "Hostile",
+  },
+  {
+    "id": 1,
+    "name": "Skeleton",
+    "health": 20,
+    "type": "Hostile",
+  },
+  {
+    "id": 2,
+    "name": "Creeper",
+    "health": 20,
+    "type": "Hostile",
+  },
+  {
+    "id": 3,
+    "name": "Spider",
+    "health": 16,
+    "type": "Hostile",
+  },
+  {
+    "id": 4,
+    "name": "Pig",
+    "health": 10,
+    "type": "Passive",
+  },
+  {
+    "id": 5,
+    "name": "Cow",
+    "health": 10,
+    "type": "Passive",
+  },
+  {
+    "id": 6,
+    "name": "Sheep",
+    "health": 8,
+    "type": "Passive",
+  },
+  {
+    "id": 7,
+    "name": "Chicken",
+    "health": 4,
+    "type": "Passive",
+  },
+];
+
+export default mobData;
