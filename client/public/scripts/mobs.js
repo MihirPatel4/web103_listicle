1cfe11e382539993f83676dfc739d327be9a6010
Mihir Patel <mk04patel@gmail.com>
2026-09-18 12:19:54 -0500

    Window is now sent to 404 page if route does not exist

--- client/public/scripts/mobs.js
+++ client/public/scripts/mobs.js
@@ -46,4 +46,11 @@ const renderMobs = async () => {
   }
 };
 
-renderMobs();
+const requestedURL = window.location.href.split('/').pop();
+
+if (requestedURL) {
+  window.location.href = '../404.html';
+}
+else {
+  renderMobs();
+}
