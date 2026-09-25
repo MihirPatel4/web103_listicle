ef47006dc45639c5e47d80610b8e4de140c40eb1
Mihir Patel <mk04patel@gmail.com>
2026-09-24 22:01:50 -0500

    /mobs endpoint now uses getMobs controller function

--- server/routes/mobRoutes.js
+++ server/routes/mobRoutes.js
@@ -1,16 +1,14 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import mobData from '../data/mobs.js'
+import MobController from '../controllers/mobController.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.status(200).json(mobData);
-});
+router.get('/', MobController.getMobs);
 
 router.get('/:mobName', (req, res) => {
   res.status(200).sendFile(path.resolve(__dirname, '../public/mob.html'));
