a6bc23974bed8edbce44e84e57670033acaf4a08
Mihir Patel <mk04patel@gmail.com>
2026-09-24 22:01:19 -0500

    Added getMobs function

---
+++ server/controllers/mobController.js
@@ -0,0 +1,18 @@
+import { pool } from '../config/database.js';
+
+const getMobs = async (req, res) => {
+  try {
+    const results = await pool.query(`
+      SELECT *
+      FROM mobs
+      ORDER BY id ASC
+      `
+    );
+    res.status(200).json(results.rows);
+  }
+  catch (err) {
+    res.status(409).json({ error: err.message });
+  }
+};
+
+export default { getMobs };
